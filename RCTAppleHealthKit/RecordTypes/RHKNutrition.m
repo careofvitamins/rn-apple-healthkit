fcc14362cbbaef1d7e8955b156f1c4ea0934b348
Ryan Bright <ryan@ryanbright.me>
2018-04-12 15:29:31 -0400

    Use the getSamples method for fetching Biotin samples

--- RCTAppleHealthKit/RecordTypes/RHKNutrition.m
+++ RCTAppleHealthKit/RecordTypes/RHKNutrition.m
@@ -15,39 +15,11 @@
 
 @implementation RCTAppleHealthKit (Nutrition)
 
+- (void)nutrition_getBiotinSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
 {
-    NSUInteger limit = [RCTAppleHealthKit uintFromOptions:input key:@"limit" withDefault:HKObjectQueryNoLimit];
-    BOOL ascending = [RCTAppleHealthKit boolFromOptions:input key:@"ascending" withDefault:false];
-    NSDate *startDate = [RCTAppleHealthKit dateFromOptions:input key:@"startDate" withDefault:nil];
-    NSDate *endDate = [RCTAppleHealthKit dateFromOptions:input key:@"endDate" withDefault:[NSDate date]];
-    
-    if(startDate == nil) {
-        callback(@[RCTMakeError(@"startDate is required in options", nil, nil)]);
-        return;
-    }
-    
-    HKQuantityType *quantityType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDietaryBiotin];
-    HKUnit *unit = [HKUnit unitFromString:@"mcg"];
-    NSPredicate * predicate = [RCTAppleHealthKit predicateForSamplesBetweenDates:startDate endDate:endDate];
-    
-    [self fetchQuantitySamplesOfType:quantityType
-                                unit:unit
-                           predicate:predicate
-                           ascending:ascending
-                               limit:limit
-                          completion:^(NSArray *results, NSError *error) {
-                              if(results){
-                                  callback(@[[NSNull null], results]);
-                                  return;
-                              } else {
-                                  NSLog(@"error biotin samples: %@", error);
-                                  callback(@[RCTMakeError(@"error getting biotin samples", nil, nil)]);
-                                  return;
-                              }
-                          }];
+    return [self getSamples:@"biotin" input:input callback:callback];
 }
 
-- (void)nutrition_getBiotinSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
 - (void)saveFood:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
 {
     NSString *foodNameValue = [RCTAppleHealthKit stringFromOptions:input key:@"foodName" withDefault:nil];
